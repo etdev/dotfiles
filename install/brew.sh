499baee122bb69e757b4964fc2fa91955aefede5
Eric Turner <ericturnerdev@gmail.com>
2017-05-11 12:01:55 +0900

    Updaaate brew packages

--- install/brew.sh
+++ install/brew.sh
@@ -9,18 +9,18 @@ command -v brew >/dev/null 2>&1 || { \
 # tap repos
 while read repo_name; do
   echo $repo_name;
-  #brew tap $repo_name;
-done <brew_repos
+  brew tap $repo_name;
+done <brew_repos.txt
 brew tap homebrew/versions
 
 # install packages
 while read package; do
   echo $package;
-  #brew install $package;
-done <brew_packages
+  brew install $package;
+done <brew_packages.txt
 
 # install cask packages
-#while read application; do
-  #echo $application;
-  #brew cask install $application;
-#done <brew_cask
+while read application; do
+  echo $application;
+  brew cask install $application;
+done <brew_cask.txt
