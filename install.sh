5222551b048e1481dd6504dd439f34a58b26819a
Eric Turner <ericturnerdev@gmail.com>
2018-03-09 13:15:42 +0900

    Make some adjustments

--- install.sh
+++ install.sh
@@ -13,31 +13,7 @@ fi
 
 # install mac components
 if [ "$platform" == "mac" ]; then
-  # brew
-  sh ./install/brew.sh
+  sh ./install/mac.sh
+elif [ "$platform" == "linux" ]; then
+  sh ./install/linux.sh
 fi
-
-# switch to zsh
-chsh -s $(which zsh)
-exec zsh
-
-# prezto
-echo "Installing prezto"
-git clone --recursive https://github.com/sorin-ionescu/prezto.git "${ZDOTDIR:-$HOME}/.zprezto"
-
-setopt EXTENDED_GLOB
-for rcfile in "${ZDOTDIR:-$HOME}"/.zprezto/runcoms/^README.md(.N); do
-  ln -s "$rcfile" "${ZDOTDIR:-$HOME}/.${rcfile:t}"
-done
-
-# migrate files using rcup
-env RCRC=$HOME/dotfiles/rcrc rcup
-
-# set zsh theme
-echo "Installing custom zsh prompt"
-/bin/cp zsh/agnoster.zsh-theme $HOME/.zprezto/modules/prompt/external/agnoster/agnoster.zsh-theme
-
-# powerline fonts
-echo "Installing patched powerline fonts"
-git clone git@github.com:powerline/powerline_fonts.git
-sh powerline_fonts/install.sh
