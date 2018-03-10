f4ba4376e6b8038d37539544ad7504f62fdc1be0
Eric Turner <ericturnerdev@gmail.com>
2018-03-10 11:34:41 +0900

    Remove bluebird

--- index.js
+++ index.js
@@ -1,24 +1,21 @@
 const emoji = require('node-emoji')
 const fs = require('fs')
-const bbPromise = require('bluebird');
 const inquirer = require('inquirer')
 const config = require('./config')
 const command = require('./lib_node/command')
 
 const installPackages = function(type){
   console.info(emoji.get('coffee'), ' installing '+type+' packages')
-  return bbPromise.map(
-    config[type],
-    function(item){
+  config[type].map(function(itemArr){
+    itemArr.map(function(item) {
       console.info(type+':', item)
       command('. lib_sh/echos.sh && . lib_sh/requirers.sh && require_'+type+' ' + item, __dirname, function(err, out) {
         if(err) console.error(emoji.get('fire'), err)
       })
-    },
-    { concurrency: 5 }
-  );
+    })
+  })
 }
 
 installPackages('brew')
-  .then(installPackages('cask'))
-  .then(installPackages('npm'))
+installPackages('cask')
+installPackages('npm')
